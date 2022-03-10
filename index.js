6e6c7a4d57121f3264623452c007ff1d83a30ab2
Will Omonaye <96916426+Omonayeg@users.noreply.github.com>
2022-03-10 23:28:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var n = Math.floor(Math.random() * 100) + 1;
+var yourName = document.querySelector(".yourname").value;
+var theirName = document.querySelector(".theirname").value;
+
+
+function loveCalculator() {
+ if (n > 75) {
+document.querySelector(".textresult").innerHTML = yourName + " " + theirName +  "Your Love score is " + n + "%." + " Your Love can move mountains, Hold on to each other.";
+ }
+ else if (n > 35 && n < 75) {
+    document.querySelector(".textresult").innerHTML = yourName + " " + theirName +  "Your Love score is " + n + "%." + " Your Love could use a little work, more understanding and Patience.";
+     }
+     if (n < 35) {
+        document.querySelector(".textresult").innerHTML = yourName + " " + theirName +  "Your Love score is " + n + "%." + " No Love found, get a job.";
+     }
+       
+     }
+    
