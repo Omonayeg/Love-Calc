b3025a8457c4c4ea824ff0ff6ba512233ee1c412
Will Omonaye <96916426+Omonayeg@users.noreply.github.com>
2022-06-24 07:26:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var yourName = document.querySelector(".yourname").value;
 var theirName = document.querySelector(".theirname").value;
 
  if (n > 75) {
-document.querySelector(".textresult").innerHTML = yourName + " " + theirName +  "Your Love score is " + n + "%." + " Your Love can move mountains, Hold on to each other.";
+document.querySelector(".textresult").innerHTML = yourName + " & " + theirName +  ", Your Love score is " + n + "%." + " Your Love can move mountains, Hold on to each other.";
  }
  else if (n > 35 && n < 75) {
-    document.querySelector(".textresult").innerHTML = yourName + " " + theirName +  "Your Love score is " + n + "%." + " Your Love could use a little work, more understanding and Patience.";
+    document.querySelector(".textresult").innerHTML = yourName + " & " + theirName +  ", Your Love score is " + n + "%." + " Your Love could use a little work, more understanding and Patience.";
      }
      if (n < 35) {
-        document.querySelector(".textresult").innerHTML = yourName + " " + theirName +  "Your Love score is " + n + "%." + " No Love found, get a job.";
+        document.querySelector(".textresult").innerHTML = yourName + " & " + theirName +  ", Your Love score is " + n + "%." + " No Love found, get a job.";
      }
        
      }
